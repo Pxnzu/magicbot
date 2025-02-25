60c69af7704b44a0eca4628fe961a395f6edc10a
AlexLovesKiwi <147126799+AlexLovesKiwi@users.noreply.github.com>
2025-02-24 22:53:34 -0500

    Pingpong

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
 const { token } = require("./config.json");
 
 const client = new Client({ 
@@ -59,8 +59,28 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
+/*
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  });
+
+});
+*/
+
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+	const ping = new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Replies with pong!")
+
+	client.application.commands.create(ping);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+	if(interaction.commandName === "ping"){
+		interaction.reply("pong!");
+	}
+});
 
 client.login(token);
