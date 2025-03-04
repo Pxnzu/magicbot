d589292c280db137d0b03a61f67682606c9587ea
Pxnzu <aorwig123@gmail.com>
2025-03-04 00:09:51 -0600

    Store usernames

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
+    username: { type: String, require: true },
     userId: { type: String, require: true, unique: true },
     serverId: { type: String, require: true},
     magicTokens: { type: Number, default: 0 },
